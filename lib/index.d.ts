0e15ffd982c000879300d33184a292400dc0a6b3
Ángel González Vázquez <contact@angelnext.dev>
2023-11-19 10:49:12 +0100

    chore: Add CI and build

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Creates and returns a new Rubiks instance.
- * @returns {Rubiks} a new Rubiks logger instance.
- */
-export function rubiks(): Rubiks;
-export * from "./levels.js";
-export * from "./modifiers.js";
-export { Rubiks };
-import { Rubiks } from "./rubiks.js";
